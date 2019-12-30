afd2ea4512464206b8b4d95c7a53c877bd2acc7c
kareem osman <58007559+kareem-osman@users.noreply.github.com>
2019-12-30 08:55:41 +0200

    30-12-2019
    add charts

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
 $(function(){
     // ----open-----
+    let xr=1
 $(".bt-v").click(function(){
-    $("#contact").css("left","0")
-    $(".bt-v").removeClass("hvr-bubble-float-right")
+    if(xr==1)
+    {
+        $("#contact").css("left","0")
+        $(".bt-v").removeClass("hvr-bubble-float-right")
+        xr=2
+    }else
+    {
+        $("#contact").css("left","-300px")
+        $(".bt-v").addClass("hvr-bubble-float-right")
+        xr=1
+    }
 })
 $('[href="#contacts"]').click(function(){
-    $("#contact").css("left","0")
-    $(".bt-v").removeClass("hvr-bubble-float-right")
+    if(xr==1)
+    {
+        $("#contact").css("left","0")
+        $(".bt-v").removeClass("hvr-bubble-float-right")
+        xr=2
+    }else
+    {
+        $("#contact").css("left","-300px")
+        $(".bt-v").addClass("hvr-bubble-float-right")
+        xr=1
+    }
 })
 // -------close----
-$('[href="#contacts"]').dblclick(function(){
-    
-    $("#contact").css("left","-300px")
-    $(".bt-v").addClass("hvr-bubble-float-right")
-})
-$(".bt-v").dblclick(function(){
-    $("#contact").css("left","-300px")
-    $(".bt-v").addClass("hvr-bubble-float-right")
-})
 $("#contact span ").click(function(){
     $("#contact").css("left","-300px")
     $(".bt-v").addClass("hvr-bubble-float-right")
+    xr=1
 })
 // -------finsh-----------------------------------------------------
 
@@ -93,4 +104,75 @@ $("#courses "+ cat_v).css("display","none")
 
 })
 
+
+
+
+
 })
+
+new Chart(document.getElementById("horizontalBar"), {
+    "type": "horizontalBar",
+    "data": {
+    "labels": [ "HTML 5.0", "CSS3", "JAVASCRIPT", "JQUERY", "BOOTSTRAP", "SASS","PHP","SQL","PHOTOSOP"],
+    "datasets": [{
+    "label": "DEVLOPER SKILLS",
+    "data": [90, 85, 80, 75, 70, 80, 75,70,75],
+    "fill": false,
+    "backgroundColor": ["rgba(255, 99, 132, 0.8)", "rgba(255, 159, 64, 0.7)",
+    "rgba(255, 205, 86, 0.7)", "rgba(75, 192, 192, 0.7)", "rgba(54, 162, 235, 0.8)",
+    "rgba(153, 102, 255, 0.8)", "rgba(201, 203, 207, 0.8)", "rgba(255, 159, 64, 0.6)","rgba(75, 192, 192, 0.7)"
+    ],
+    "borderColor": ["rgb(255, 99, 132)", "rgb(255, 159, 64)", "rgb(255, 205, 86)",
+    "rgb(75, 192, 192)", "rgb(54, 162, 235)", "rgb(153, 102, 255)", "rgb(201, 203, 207)", "rgb(255, 159, 64)", "rgb(54, 162, 235)"
+    ],
+    "borderWidth": 1
+    }]
+    },
+    "options": {
+    "scales": {
+    "xAxes": [{
+    "ticks": {
+    "beginAtZero": true
+    }
+    }]
+    }
+    }
+    });
+
+    // ------------------------
+    //bar
+var ctxB = document.getElementById("barChart").getContext('2d');
+var myBarChart = new Chart(ctxB, {
+type: 'bar',
+data: {
+labels: ["AUTOCAD", "CONSTRUCTION ", "FIXING ","REPARING"],
+datasets: [{
+label: 'CIVIL ENGINEERING',
+data: [90, 80, 90,95],
+backgroundColor: [
+'rgba(255, 99, 132, 0.2)',
+'rgba(75, 192, 192, 0.2)',
+'rgba(54, 162, 235, 0.2)',
+'rgba(153, 102, 255, 0.2)'
+],
+borderColor: [
+'rgba(255,99,132,1)',
+'rgba(54, 162, 235, 1)',
+'rgba(54, 162, 235, 1)',
+'rgba(153, 102, 255, 1)'
+],
+borderWidth: 1
+}]
+},
+options: {
+scales: {
+yAxes: [{
+ticks: {
+beginAtZero: true
+}
+}]
+}
+}
+});
+
+
